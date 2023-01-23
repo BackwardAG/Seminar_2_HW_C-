5adc8e079a3a29481c30837d7e50e97cf31b859c
Armen G <ar.gukasov@gmail.com>
2023-01-23 11:35:28 +0300

    Added 3 files with 3 tasks for HW

---
+++ HW_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Random rand = new Random();
+int randNumber = rand.Next(100, 1000);
+Console.WriteLine(randNumber);
+
+System.Console.WriteLine((randNumber / 10) % 10);
+
+// Console.WriteLine("Введите трехзначное число, ");
+
+// string number = Console.ReadLine();
+// Console.WriteLine(number[1]);
+
---
+++ HW_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите число от 1 до 7, ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+    System.Console.WriteLine("Да");
+}
+else if (number <= 5 && number >= 1)
+{
+    System.Console.WriteLine("Нет");
+}
+else
+{
+    System.Console.WriteLine("Нет такого дня!");
+}
+    
+
+
+
+
+
+
+
